daf54ea0d97783eaab8c16b6a966b927450c5b0e
Daniel Alvarez <dalvar13@ibero.edu.co>
2024-04-19 22:36:27 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+TITULO ENTREGA FINAL: PSICOLOGÍA ONLINE
+DESCRIPCIÓN
+Dirigida a toda la población nacional para acceder a servicios de psicología online
+
+AUTOR:
+Daniel Alvarez
---
+++ scripts/script.js
@@ -0,0 +1,15 @@
+var swiper = new Swiper(".myswiper", {
+    
+    slidesPerView: 1,
+    spaceBetween: 80,
+    grabCursor: true,
+    loop: true,
+    breakpoints : {
+        911: {
+            slidesPerView: 3,
+        }
+    }
+
+
+})
+
